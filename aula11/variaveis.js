1034cef5cfb6d1146f7fb386d7f13f65146d660f
Anderson Santana <anderslash@gmail.com>
2025-07-15 10:41:43 -0300

    first commit

---
+++ aula11/variaveis.js
@@ -0,0 +1,29 @@
+const idade = 25;
+const nome = "Alice";
+let endereco = "Bairro do Limoeiro";
+const URL = "http://escolakoru.com.br";
+
+const listaDeFrutas = (["maça", "manga", "tomate"]);
+
+const carro = {
+  marca: "Toyota",
+  modelo: "Corolla",
+  ano: 2022,
+	cor: "Prata",
+};
+
+const a = 10;
+const b = 10;
+
+
+let carrinho = 0;
+carrinho += 1;
+
+console.log(10 !== 10)
+
+
+// console.log(`Olá, ${nome}. Vejo que você tem ${idade} anos de idade`)
+
+// console.log(carro);
+
+// console.log(carrinho);
---
+++ aula12/operadores.js
@@ -0,0 +1,5 @@
+const age = 25;
+const isLegalAge = age >= 18; // true or false
+const adultOrNot = isLegalAge ? "Adulto" : "Menor de idade";
+
+console.log(adultOrNot);
